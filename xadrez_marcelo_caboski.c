4c139da44731e47835b3dea83278fdb3cbaf5276
marcelochabowski <104426941+marcelochabowski@users.noreply.github.com>
2025-08-03 14:48:03 -0300

    xadres.1.0

--- xadrez_marcelo_caboski.c
+++ xadrez_marcelo_caboski.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // biblioteca padrão
 
 int main(){
-    char tabuleiro[8][8] =  {
+    char tabuleiro[8][8] = { // usando char para criar tabulerio
         {'T','C','B','D','R','B','C','T'},
         {'P','P','P','P','P','P','P','P'},
         {' ',' ',' ',' ',' ',' ',' ',' '},
@@ -11,6 +11,13 @@ int main(){
         {'p','p','p','p','p','p','p','p'},
         {'t','c','b','d','r','b','c','t'}
     };
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++){
+      for ( int j = 0; j < 8; j++) {
+    // criando função de repetição
+      printf(" [%c]", tabuleiro[i][j]);
+      }
+      printf("\n");
+    }
+    return 0;
 
 }
